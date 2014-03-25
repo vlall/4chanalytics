90adbc70840b5e2aa28f2c048f4a755afef77b2f
V <vishallall@live.com>
2014-03-24 22:40:40 -0400

    comment
    comment extended

--- chancrawl.py
+++ chancrawl.py
@@ -77,10 +77,10 @@ for i in range (len(contentList)):
         else:
             print space + 'Reply #' + str(j) + space +(comment)
 
-# Current bugs:
-#1) If theres more than one quote it doesn't get the reply. 
-#^ loop through list of </a> values to get all the reply 
-#2) Doesn't delete tripcodes
- #3) if you direct to a different thread, it messes up and places that as part of the reply.
-#4) treat <br> as breaks---Check
-#5) FORMAT INTO FUNCTIONS
+'''Current bugs:
+1) If theres more than one quote it doesn't get the reply. 
+^ loop through list of </a> values to get all the reply 
+2) Doesn't delete tripcodes
+3) if you direct to a different thread, it messes up and places that as part of the reply.
+4) treat <br> as breaks---Check
+5) FORMAT INTO FUNCTIONS'''
